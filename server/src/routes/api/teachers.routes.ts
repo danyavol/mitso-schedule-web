a5b9582d34a4db0d820654494f34f8bca1a5e3bf
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2022-02-09 20:28:38 +0300

    feat: Add teachers schedule endpoint

--- server/src/routes/api/teachers.routes.ts
+++ server/src/routes/api/teachers.routes.ts
@@ -1,4 +1,5 @@
 import { db } from "@database/database";
+import { DateStringFormatter } from "@services/time.service";
 import { Router } from "express";
 
 const teachers = Router();
@@ -8,4 +9,22 @@ export default teachers;
 teachers.get('/', async (req, res) => {
     const allTeachers = (await db.Teacher.find().sort({ name: 1})).map(teacher => teacher.name);
     res.json( allTeachers );
+});
+
+teachers.get('/:collection/:teacher', async (req, res) => {
+    const { collection, teacher } = req.params;
+    
+    const schedule = await db.Schedule(collection).find();
+
+    const filteredSchedule = [];
+    schedule.forEach(group => {
+        const filteredLessons = group.lessons.filter(lesson =>
+            lesson.teachers.includes(teacher)
+        );
+        filteredSchedule.push(...filteredLessons);
+    });
+
+    filteredSchedule.sort((a, b) => DateStringFormatter.getLessonTime(a.date, a.time) - DateStringFormatter.getLessonTime(b.date, b.time) );
+
+    res.json(filteredSchedule);
 });
